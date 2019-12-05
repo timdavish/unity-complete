947ada7cd63324fcf42a3cc907d2e0aea743437d
Tim Davis <timdavish@gmail.com>
2019-12-04 18:42:22 -0800

    feat: finish Terminal Hacker

--- 2_Terminal_Hacker/Assets/WM2000/Hacker.cs
+++ 2_Terminal_Hacker/Assets/WM2000/Hacker.cs
@@ -2,22 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+enum Screen
+{
+  MainMenu,
+  Password,
+  Win
+};
+
 public class Hacker : MonoBehaviour
 {
+  // Game configuration
+  string[][] passwords = {
+    new string[] {"books", "aisle", "shelf", "password", "font", "borrow"},
+    new string[] {"prisoner", "handcuffs", "holster", "uniform", "arrest"}
+  };
+
+  // Game state
+  Screen currentScreen;
+  int level;
+  string password;
+
   // Start is called before the first frame update
   void Start()
   {
     ShowMainMenu();
-  }
-
-  void ShowMainMenu()
-  {
-    Terminal.ClearScreen();
-    Terminal.WriteLine("Hello Tim");
-    Terminal.WriteLine("What would you like to hack into?");
-    Terminal.WriteLine("Press 1 for the local library");
-    Terminal.WriteLine("Press 2 for the police station");
-    Terminal.WriteLine("Enter your selection:");
   }
 
   void OnUserInput(string input)
@@ -26,13 +34,87 @@ public class Hacker : MonoBehaviour
     {
       ShowMainMenu();
     }
-    else if (input == "007")
+    else if (currentScreen == Screen.MainMenu)
     {
-      Terminal.WriteLine("Please select a level, Mr. Bond.");
+      RunMainMenu(input);
     }
-    else
+    else if (currentScreen == Screen.Password)
     {
-      Terminal.WriteLine("Please choose a valid level.");
+      RunPasswordScreen(input);
     }
+  }
+
+  void ShowMainMenu()
+  {
+    currentScreen = Screen.MainMenu;
+    Terminal.ClearScreen();
+
+    Terminal.WriteLine("Hello Tim");
+    Terminal.WriteLine("What would you like to hack into?");
+    Terminal.WriteLine("Press 1 for the local library");
+    Terminal.WriteLine("Press 2 for the police station");
+    Terminal.WriteLine("Enter your selection:");
+  }
+
+  void RunMainMenu(string input)
+  {
+    switch (input)
+    {
+      case "1":
+      case "2":
+        ShowPasswordScreen(input);
+        break;
+
+      case "007":
+        Terminal.WriteLine("Select a level, Mr. Bond.");
+        break;
+
+      default:
+        Terminal.WriteLine("Choose a valid level.");
+        break;
+    }
+  }
+
+  void ShowPasswordScreen(string input)
+  {
+    currentScreen = Screen.Password;
+    level = int.Parse(input);
+    password = passwords[level - 1][Random.Range(0, passwords[level - 1].Length)];
+
+    Terminal.ClearScreen();
+    Terminal.WriteLine("You have chosen level " + level);
+    Terminal.WriteLine("Enter your password: " + password.Anagram());
+    Terminal.WriteLine("Type \"menu\" to return to the main menu.");
+  }
+
+  void RunPasswordScreen(string input)
+  {
+    if (input == password)
+    {
+      ShowWinScreen();
+    } else
+    {
+      Terminal.WriteLine("Wrong!");
+    }
+  }
+
+  void ShowWinScreen()
+  {
+    currentScreen = Screen.Win;
+
+    Terminal.ClearScreen();
+
+    switch (level)
+    {
+      case 1:
+        Terminal.WriteLine("Well done, noob.");
+        break;
+
+      case 2:
+        Terminal.WriteLine("Well done, sir.");
+        break;
+    }
+
+    Terminal.WriteLine("Type \"menu\" to return to the main menu.");
   }
 }
